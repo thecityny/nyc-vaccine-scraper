7e434142789b8005d6deaa3ae6892c82942f249b
Will Welch <wwelch@thecity.nyc>
2021-02-16 12:48:22 -0500

    Added map scraper

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 const fs = require("fs");
 const tableauScraper = require("./tableau-scraper");
 
-async function main(path) {
+async function downloadData(path, slug, tableKey) {
   const host = "https://public.tableau.com";
-  const slug = "COVID-19VaccinationTracker/AllAdults";
 
   const config = await tableauScraper.queryConfig(host, slug);
   const [info, data] = await tableauScraper.queryData(host, config);
   const views = info["worldUpdate"]["applicationPresModel"]["workbookPresModel"]["dashboardPresModel"]["viewIds"];
   
-  const tableKey = "NYC v NON NYC overall";
-  
   if (views[tableKey]) {
     const data = await tableauScraper.queryTable(host, config, views[tableKey]);
   
-    console.log(`Saving NYC vaccine data to ${path}`);
+    console.log(`Saving "${tableKey}" from ${slug} to ${path}`);
     fs.writeFileSync(path, JSON.stringify(data, null, 2));
   } else {
-    console.error(`Vaccine table "${tableKey}" does not exist. Update the scraper with one of the following:\n ${JSON.stringify(Object.keys(views), null, 2)}`);
+    console.error(`Table "${tableKey}" does not exist. Update the scraper with one of the following:\n ${JSON.stringify(Object.keys(views), null, 2)}`);
   }
-}
+};
 
 try {
-  main("vaccine-data.json");
+  downloadData("vaccine-data.json", "COVID-19VaccinationTracker/AllAdults", "NYC v NON NYC overall");
+  downloadData("map-data.json", "COVID-19VaccinationTracker/Geography", "Map ZIP");
 } catch (e) {
   console.error(e);
 }
